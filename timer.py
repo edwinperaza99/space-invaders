44d9fd8f7a8979df2c190537505f16f8b0f959a6
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-03-03 17:38:33 -0800

    initial commit

---
+++ timer.py
@@ -0,0 +1,25 @@
+class Timer: 
+  def __init__(self, image_list, start_index=0, delta=6, looponce=False): 
+    self.image_list = image_list
+    self.delta = delta
+    self.looponce = looponce
+    self.index = start_index
+    self.time = 0
+
+  def update_index(self):
+    self.time += 1
+    if self.time >= self.delta:
+      self.index += 1
+      self.time = 0
+      if self.index >= len(self.image_list) and not self.looponce:
+        self.index = 0
+
+  def finished(self): 
+    return self.looponce and self.index >= len(self.image_list)
+  
+  def current_index(self): return self.index
+
+  def current_image(self):     # self.time = 0
+    self.update_index()
+    return self.image_list[self.index]
+  
